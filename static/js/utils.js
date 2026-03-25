22149e9eaa5e3c54b643568638d1125b5c2d1576
lukeclaude <hall1134@vandals.uidaho.edu>
2026-03-25 15:09:13 -0700

    auto: update utils.js

---
+++ static/js/utils.js
@@ -0,0 +1,11 @@
+function escHtml(s) {
+    return s.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
+}
+
+function toast(msg, type = 'success') {
+    const el = document.createElement('div');
+    el.className = `toast ${type}`;
+    el.textContent = msg;
+    document.getElementById('toasts').appendChild(el);
+    setTimeout(() => el.remove(), 3200);
+}
